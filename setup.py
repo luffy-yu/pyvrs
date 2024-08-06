b90a0aacc12c505bd2b565b221dbde9e3a7e51a3
Liuchuan Yu <lyu20@gmu.edu>
2024-08-06 19:44:19 -0400

    buildable on win11 and vs2019

--- setup.py
+++ setup.py
@@ -80,6 +80,7 @@ class CMakeBuild(build_ext):
             f"-DCMAKE_BUILD_TYPE={cfg}",
             f"-DCMAKE_LIBRARY_OUTPUT_DIRECTORY={extdir}",
             f"-DPYTHON_EXECUTABLE={sys.executable}",
+	    f"-DCMAKE_TOOLCHAIN_FILE=D:/Projects/Code/vcpkg/scripts/buildsystems/vcpkg.cmake",
         ]
         build_args = ["--target", os.path.basename(ext.name)]
 
